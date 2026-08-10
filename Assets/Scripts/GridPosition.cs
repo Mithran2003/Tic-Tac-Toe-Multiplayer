8154747ec98a274c8f6f50e498754034de2ef451
Mithran2003 <mithransaini5@gmail.com>
2026-08-10 19:54:05 +0530

    Added Click Detection and Game Manager

--- Assets/Scripts/GridPosition.cs
+++ Assets/Scripts/GridPosition.cs
@@ -1,6 +1,13 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class GridPosition : MonoBehaviour
+public class GridPosition : MonoBehaviour,IPointerDownHandler
 {
-    
+    [SerializeField] private int X;
+    [SerializeField] private int Y;
+    public void OnPointerDown(PointerEventData eventData) 
+    {
+        Debug.Log($"Pointer Click on {X},{Y}");
+        GameManager.Instance.ClickedOnGridPosition(X,Y);    
+    }
 }
