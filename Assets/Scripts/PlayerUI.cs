399c2d69bc36664e1ad7844cead68c13648f0f38
Mithran2003 <mithransaini5@gmail.com>
2026-08-11 18:47:11 +0530

    Added Player UI

---
+++ Assets/Scripts/PlayerUI.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class PlayerUI : MonoBehaviour
+{
+   [SerializeField] private GameObject CrossArrowGameObject;
+   [SerializeField] private GameObject CircleArrowGameObject;
+   [SerializeField] private GameObject CrossYouTMPGameObject;
+   [SerializeField] private GameObject CircleYouTMPGameObject;
+}
