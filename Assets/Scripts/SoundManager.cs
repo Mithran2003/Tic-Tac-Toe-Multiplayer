37036b98b1ca5af684680e564635a06a3dfd808c
Mithran2003 <mithransaini5@gmail.com>
2026-08-22 19:22:31 +0530

    added sound effects and music

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -4,10 +4,27 @@ using UnityEngine;
 public class SoundManager : MonoBehaviour
 {
     [SerializeField] private Transform placeSFXprefab;
+    [SerializeField] private Transform WinSFXprefab;
+    [SerializeField] private Transform LoseSFXprefab;
 
     private void Start()
     {
         GameManager.Instance.OnPlacedObject+=GameManager_OnPlacedObject;
+        GameManager.Instance.OnGameWin+=GameManager_OnGameWin;
+    }
+
+    private void GameManager_OnGameWin(object sender,GameManager.OnGameWinEventArgs e) 
+    {
+        if(GameManager.Instance.GetLocalPlayerType()==e.winPlayerType)
+        {
+            Transform sfxTransform=Instantiate(WinSFXprefab);    
+            Destroy(sfxTransform.gameObject,5f);
+        } 
+        else
+        {
+            Transform sfxTransform=Instantiate(LoseSFXprefab);    
+            Destroy(sfxTransform.gameObject,5f);
+        }   
     }
 
     private void GameManager_OnPlacedObject(object sender,EventArgs e) 
