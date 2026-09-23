1ec50464fe332f4ec2f4b190aeb0fc434f841082
Mithran2003 <mithransaini5@gmail.com>
2026-09-23 16:24:48 +0530

    minor polishiing

--- Assets/LobbyTutorial/Scripts/EditPlayerName.cs
+++ Assets/LobbyTutorial/Scripts/EditPlayerName.cs
@@ -17,14 +17,14 @@ public class EditPlayerName : MonoBehaviour {
     [SerializeField] private TextMeshProUGUI playerNameText;
 
 
-    private string playerName = "Code Monkey";
+    private string playerName = "Unkown Player";
 
 
     private void Awake() {
         Instance = this;
 
         GetComponent<Button>().onClick.AddListener(() => {
-            UI_InputWindow.Show_Static("Player Name", playerName, "abcdefghijklmnopqrstuvxywzABCDEFGHIJKLMNOPQRSTUVXYWZ .,-", 20,
+            UI_InputWindow.Show_Static("Player Name", playerName, "abcdefghijklmnopqrstuvxywzABCDEFGHIJKLMNOPQRSTUVXYWZ1234567890 .,-_", 25,
             () => {
                 // Cancel
             },
